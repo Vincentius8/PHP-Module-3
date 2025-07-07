83d2f1066169cf7a683eef9dc64229b0210af07e
John Vincent Pangilinan <johnvincentppangilinan@gmail.com>
2025-07-07 19:58:05 +0800

    Add files via upload
    A practice code of Logical Operators using Logical Operator, Arithmetic Operator and Comparison Operator

---
+++ combine_power_core.php
@@ -0,0 +1,70 @@
+<?php
+//Arithmetic Operators excercise 
+/*
+$quiz= 90;
+$exam = 90;
+$project = 97;
+$attendance = 100;
+$recitation = 95;
+
+$total = $quiz + $exam + $project + $attendance + $recitation;
+$average = $total / 5;
+
+echo "Quiz Score: " . $quiz . "<br>";
+echo "Exam Score: " . $exam . "<br>";
+echo "Project Score: " . $project . "<br>";
+echo "Attendance Score: " . $attendance . "<br>";
+echo "Recitation Score: " . $recitation . "<br>";
+
+echo "Total  Score: " . $total . "<br>";
+echo "Student Average: " . $average . "<br>";*/
+
+/*
+//using identical operator in Comparison Operator
+$username = "Admin";
+$inputusername = "Admin"; 
+
+if($inputusername===$username){
+    echo  "Welcome, Admin!";
+}else{
+    echo "Login Failed!";
+}*/
+
+/*
+//Logical Operator Sample Code:
+$username = "Admin";
+$password = "Admin";
+
+$userinput = "Admsjw";
+$inputpass = "Adminskmj";
+
+if ($username===$userinput && $password===$inputpass){
+    echo "Welcome, Admin!!";
+} elseif($username!==$userinput && $password===$inputpass){
+    echo "Invalid Username check it first!";
+} elseif($username===$userinput && $password!==$inputpass){
+    echo "Incorrect Password check it first";
+} else{
+    echo "Login Failed";
+}*/
+
+/*
+//Logical Operator Sample Code:
+$username = "Admin";
+$password = "Admin";
+
+$userinput = "";
+$inputpass = "";
+
+if ($username===$userinput && $password===$inputpass){
+    echo "Welcome, Admin!!";
+} elseif($username===$userinput && $password!==$inputpass){
+    echo "Incorrect Password check it first";
+} elseif($username !==$userinput && $password===$inputpass){
+    echo "Invalid Username";
+} elseif ($userinput !== $username || $password!==$inputpass){
+    echo "Invalid Username or Password";
+} else {
+    echo "Login Failed";
+}*/
+?>
